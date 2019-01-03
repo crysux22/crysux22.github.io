1aecc41a6006f037522afad52f4f345bcc43f7b5
crysux22 <maldonalmelvin@gmail.com>
2019-01-02 23:24:45 -0500

    Fresh Upload

--- Jousting.js
+++ Jousting.js
@@ -901,9 +901,9 @@ ApplicationMain.create = function(config) {
 	ManifestResources.init(config);
 	var _this = app.meta;
 	if(__map_reserved["build"] != null) {
-		_this.setReserved("build","72");
+		_this.setReserved("build","74");
 	} else {
-		_this.h["build"] = "72";
+		_this.h["build"] = "74";
 	}
 	var _this1 = app.meta;
 	if(__map_reserved["company"] != null) {
@@ -66211,7 +66211,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 496435;
+	this.version = 11920;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = ["lime","utils","AssetCache"];
